388a3300d66139848063ee24c532e4e4c17bd105
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-06-20 16:20:41 +0530

    added table functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 var button = document.querySelector(".submit");
 var city = document.querySelector(".city_name");
-var nam = document.querySelector(".name");
-var desc = document.querySelector(".desc");
-var temp = document.querySelector(".temp");
-var wind = document.querySelector(".wind_speed");
+
 // Event Handler
 button.addEventListener('click', function (e) {
     e.preventDefault();
@@ -16,9 +13,38 @@ function loadData() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+city.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
         .then(response => response.json())
         .then(data => {
-            nam.innerHTML = "City Name : " + data['name'];
-            desc.innerHTML = "Temperature : " + data['main']['temp'] + "Kelvin";
-            temp.innerHTML = "Description : " + data['weather']['0']['description'];
-            wind.innerHTML = "Wind Speed : " + data['wind']['speed'] + "m/s";
+
+
+            var tbl=document.getElementById('tbl');
+            
+            // Insertion of rows
+            var x=tbl.insertRow(1); 
+
+            /* ----------------Insertion of cells(columns)----------------------*/
+
+            // Country Name
+            x.insertCell().innerHTML=data['name'];
+            tbl.rows[1].cells[0].style.color="Blue";
+
+
+
+            // Confirmed cases
+            x.insertCell().innerHTML=data['main']['temp'] + "Kelvin";
+            tbl.rows[1].cells[1].style.color="green";
+
+            
+            x.insertCell().innerHTML=data['weather']['0']['description'];
+            tbl.rows[1].cells[2].style.color="red";
+
+            
+
+            // Total Deaths
+            x.insertCell().innerHTML=data['wind']['speed'] + "m/s";
+            tbl.rows[1].cells[3].style.color="#0984e3";
+
+            
+        
+            
+           
         });
 }
